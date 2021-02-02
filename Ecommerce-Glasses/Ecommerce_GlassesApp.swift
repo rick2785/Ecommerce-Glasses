0c612e6846bd96c399d07cd79768cf2cdb11f82d
rick2785 <rjhrabowskie@gmail.com>
2021-02-02 07:07:19 -0600

    Initial Commit

---
+++ Ecommerce-Glasses/Ecommerce_GlassesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ecommerce_GlassesApp.swift
+//  Ecommerce-Glasses
+//
+//  Created by RJ Hrabowskie on 2/2/21.
+//
+
+import SwiftUI
+
+@main
+struct Ecommerce_GlassesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
